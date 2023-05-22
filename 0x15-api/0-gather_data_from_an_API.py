9d04270195a6beb8b51205c749fe9cd862dee893
MarkTinega <martinozas@gmail.com>
2023-05-22 19:03:44 +0300

    Debugging task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,12 +7,14 @@ import sys
 
 BASE_URL = 'https://jsonplaceholder.typicode.com/'
 
+
 def request(resource, params=None):
     """Retrieve data from API"""
     url = BASE_URL + resource
     response = requests.get(url, params=params)
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -28,6 +30,8 @@ if __name__ == "__main__":
     total_tasks = len(tasks)
     completed_tasks = len(tasks_completed)
 
-    print(f'Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):')
+    print('Employee {} is done with tasks ({}/{}):'.format(employee_name,
+                                                           completed_tasks,
+                                                           total_tasks))
     for task in tasks_completed:
         print(f'     {task["title"]}')
