af17114b8a10a72dd894de0795317760b516850d
MarkTinega <martinozas@gmail.com>
2023-06-08 16:02:34 +0300

    Addressing the invalid subreddit arguments of number_of_subscribers and top_ten function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,9 +14,8 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": user_agent}
     response = requests.get(base_url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        subscribers = response.json()["data"]["subscribers"]
+        return subscribers
+    elif response.status_code == 302:
         return 0
-
-    subscribers = response.json()["data"]["subscribers"]
-
-    return subscribers
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,31 +13,15 @@ def top_ten(subreddit):
     base_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     user_agent = "0x16. API advanced"
     headers = {"User-Agent": user_agent}
+    response = requests.get(base_url, headers=headers, allow_redirects=False)
 
-    post_titles = []  # List to store post titles
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
 
-    after = None  # Variable to track the "after" value for pagination
+        for post in posts:
+            title = post["data"]["title"]
+            print(title)
 
-    while len(post_titles) < 10:  # Fetching a total of 10 posts
-        url = base_url
-        if after:
-            # Append "after" parameter for pagination
-            url += "&after={}".format(after)
-
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code != 200:
-            print("None")
-        data = response.json()["data"]
-        comments = data["children"]
-        for comment in comments:
-            title = comment["data"]["title"]
-            post_titles.append(title)
-
-        after = data["after"]  # Update "after" value for the next page
-
-        if not after:
-            break  # Break the loop if there are no more pages
-
-    for title in post_titles:
-        print(title)
+    elif response.status_code == 302:
+        print("None")
