f546b73020d067638642bc23e23ef4316e46b25f
MarkTinega <martinozas@gmail.com>
2023-06-15 12:20:11 +0300

    Reconfigured Nginx to restart only when notified and added a comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
+# Puppet file that configures nginx to handle multiple requests and restarts it when notified
 file { '/etc/default/nginx':
-  content => 'ULIMIT="-n 4096"',
-  notify  => Service['nginx'],
+  content      => 'ULIMIT="-n 4096"',
+  notify       => Service['nginx'],
+  refreshonly  => true,
 }
 
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/etc/default/nginx'],
+  ensure => 'running',
+  enable => true,
 }
